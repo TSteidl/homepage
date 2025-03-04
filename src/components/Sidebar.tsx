9317a01099ab429533d46d53d49dbd2469ec8827
Tina Steidl <tsteidl@duck.com>
2025-03-04 17:02:54 +0100

    Added a basic sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,51 @@
 import { Link } from "@tanstack/react-router";
 import { ModeToggle } from "./mode-toggle";
+import { Button } from "./ui/button";
+import { GithubIcon } from "lucide-react";
 
 export default function Sidebar() {
   return (
-    <header className="p-2 flex gap-2 flex-col justify-center w-full">
-      <nav className="flex flex-col">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+    <header className="p-2 flex flex-col w-full justify-between">
+      <nav className="flex flex-col pt-12">
+        <div className="flex flex-col">
+          <Link to="/">
+            <Button className="rounded-none skew-y-12">
+              <div className="text-lg">Home</div>
+            </Button>
+          </Link>
+          <Link to="/about">
+            <Button className="rounded-none skew-y-12 translate-y-2">
+              <div className="text-lg">About</div>
+            </Button>
+          </Link>
+        </div>
+        <div className="gap-2">
+          <a
+            href="https://github.com/TSteidl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button
+              size={"icon"}
+              className="rounded-none skew-y-12 translate-y-2"
+            >
+              <GithubIcon />
+            </Button>
+          </a>
+          <a
+            href="https://www.threads.net/@tina.steidl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button
+              size={"icon"}
+              className="rounded-none skew-y-12 translate-y-4.5 translate-x-2"
+            >
+              <ModeToggle />
+            </Button>
+          </a>
+        </div>
       </nav>
-      <ModeToggle />
     </header>
   );
 }
